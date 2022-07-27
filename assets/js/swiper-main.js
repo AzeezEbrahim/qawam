3d45f177cab1bdf9733d774983eda4ff0692ddaf
Abdulaziz Hamid Ebrahim <azeez.h.ebrahim@gmail.com>
2022-07-27 05:52:58 +0300

    bugs fixes

--- assets/js/swiper-main.js
+++ assets/js/swiper-main.js
@@ -1,9 +1,9 @@
 // SWIPER CONFIG (divce slider)
 var reviewsSwiper = new Swiper(".swiper", {
-  slidesPerView: window.innerWidth < 1250 ? 1 : 2.45,
+  slidesPerView: window.innerWidth < 850 ? 1 : 2.45,
   // slide size fit to the screen
   slideSize: window.innerWidth < 700 ? window.innerWidth*0.5 : window.innerWidth,
-  spaceBetween:300,
+  spaceBetween: 300,
   grabCursor: true,
   loop: false,
   speed: 1000,
@@ -19,24 +19,9 @@ var reviewsSwiper = new Swiper(".swiper", {
 // start from second slide
 reviewsSwiper.slideTo(1, false,false);
 
-// SWIPER CONFIG (comments slider)
-var reviewsSwiper2 = new Swiper(".swiper2", {
-  spaceBetween:300,
-  grabCursor: true,
-  loop: true,
-  speed: 600,
-  centeredSlides: true,
-  clickable: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
   // on window resize if width is less than 1000px, sliderperview is set to 1
   window.addEventListener("resize", function() {
-    if (window.innerWidth < 1250) {
+    if (window.innerWidth < 850) {
       reviewsSwiper.params.slidesPerView = 1;
     } else {
       reviewsSwiper.params.slidesPerView = 2.45;
